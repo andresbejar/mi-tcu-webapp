ab8d6a919d7055c9221423dab53dc99c244ec452
Andres Bejarano <andresbejar@gmail.com>
2016-07-10 12:09:55 -0600

    Se agrega vista + controlador de proyecto individual.

---
+++ app/scripts/controllers/proyecto.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = angular.module('miTcuApp');
+
+app.controller('ProyectoCtrl', function($scope, $routeParams, $http){
+
+	$scope.proyectoId = $routeParams.id;
+	$http.get('/api/proyectos/' + $scope.proyectoId).then(function(res){
+		if(res.status === 200){
+			$scope.proyecto = res.data;
+		}
+	}, function(err){
+		$scope.message = err;
+	});
+
+});
