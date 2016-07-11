3b79c4b0a7992f6115d5349a1058d816a52cc6df
jeffryvenegas <jeffvene@gmail.com>
2016-07-10 18:11:27 -0600

    Cambios en proyectos

--- app/scripts/controllers/proyectos.js
+++ app/scripts/controllers/proyectos.js
@@ -1,10 +1,26 @@
 'use strict';
 
-angular.module('miTcuApp')
-  .controller('ProyectosCtrl', function ($scope, $http) {
+angular.module('miTcuApp').controller('ProyectosCtrl', function ($scope, $http) {
 
     $http.get('/api/proyectos').success(function(proyectos) {
+      console.log(proyectos);
       $scope.proyectos = proyectos;
     });
 
   });
+  // 'use strict';
+  // var app = angular.module('miTcuApp');
+  //
+  // app.controller('ProyectosCtrl', function ($scope, $http) {
+  //
+  //   $scope.proyectos = []
+  //   $http.get('/api/proyectos').then(function(res){
+  //     if(res.status === 200){
+  //       $scope.proyectos = res.data;
+  //       $scope.message = 'Success';
+  //     }
+  //   }).then(function(err){
+  //     $scope.message = err;
+  //   });
+  //
+  // });
